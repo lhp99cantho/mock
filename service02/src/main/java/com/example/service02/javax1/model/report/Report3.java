3d05ab9668776bcac566a06b81379599895b51a5
Eugene Wang <125450679+magmance@users.noreply.github.com>
2023-10-17 16:57:39 +0700

    Merge code cho User

--- service02/src/main/java/com/example/service02/javax1/model/report/Report3.java
+++ service02/src/main/java/com/example/service02/javax1/model/report/Report3.java
@@ -1,4 +1,21 @@
 package com.example.service02.javax1.model.report;
 
+import com.example.service02.javax1.model.store.product.Category;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Report3 {
+
+    @Id
+    private Category group;
+
+    private double sum;
+
+    private long count;
+
 }
